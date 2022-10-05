dea5db64a987e6600efacbc90e357d72fb357978
dlekdlsll <dtp05093@naver.com>
2022-10-05 22:05:34 +0900

    WebMvcConfig 생성

---
+++ SixthWeek/src/main/java/com/example/fourthweek2/config/WebMvcConfig.java
@@ -0,0 +1,7 @@
+package com.example.fourthweek2.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+}
