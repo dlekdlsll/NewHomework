ef6d3ec1e11749d1c0ee62261408013db32146cf
dlekdlsll <dtp05093@naver.com>
2022-10-05 22:06:19 +0900

    어노테이션 추가

--- SixthWeek/src/main/java/com/example/fourthweek2/config/WebMvcConfig.java
+++ SixthWeek/src/main/java/com/example/fourthweek2/config/WebMvcConfig.java
@@ -1,7 +1,9 @@
 package com.example.fourthweek2.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+@Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
 }
