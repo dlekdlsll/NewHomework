bb964502b029b1cbbea01f1c5ff73c5e570e4fa5
dlekdlsll <dtp05093@naver.com>
2022-10-05 22:09:36 +0900

    swaggerConfig와 충돌할 경우

--- SixthWeek/src/main/java/com/example/fourthweek2/config/swaggerConfig.java
+++ SixthWeek/src/main/java/com/example/fourthweek2/config/swaggerConfig.java
@@ -2,6 +2,7 @@ package com.example.fourthweek2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,6 +11,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
+//public class swaggerConfig extends WebMvcConfigurationSupport {   // swaggerconfig와 WebMbcConfig가 충돌할 경우
 public class swaggerConfig {
     @Bean
     public Docket api(){
@@ -29,5 +31,4 @@ public class swaggerConfig {
                 .version("1.0")
                 .build();
     }
-
 }
