aa6233c0fb6e479bcb6314bbe9024c2ea73e9dac
DAIN <83645643+dlekdlsll@users.noreply.github.com>
2022-10-11 21:49:10 +0900

    Update and rename SixthWeek/SixthWeek/src/test/java/com/example/fourthweek2/FourthWeek2ApplicationTests.java to SixthWeek/SixthWeek/src/test/java/com/example/sixthweek/SixthWeekApplicationTests.java

--- SixthWeek/SixthWeek/src/test/java/com/example/sixthweek/SixthWeekApplicationTests.java
+++ SixthWeek/SixthWeek/src/test/java/com/example/sixthweek/SixthWeekApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FourthWeek2ApplicationTests {
+class SixthWeekApplicationTests {
 
 	@Test
 	void contextLoads() {
